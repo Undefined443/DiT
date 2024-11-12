e8fa2d20f8e27ff977e9cde4afdf729d2b029259
Xiao Li <xiaoli3397@gmail.com>
2024-11-12 23:12:24 +0800

    update

--- models.py
+++ models.py
@@ -251,10 +251,8 @@ class DiT(nn.Module):
 
         if x_noised is not None:
             q_sample = q_sample(x)
-            print("x_noised:", x_noised.shape, x_noised.mean().item(), x_noised.std().item())
-            print("q_sample:", q_sample.shape, q_sample.mean().item(), q_sample.std().item())
             assert torch.allclose(q_sample, x_noised, rtol=1e-5), "q_sample 结果和 x_noised 不相同"
-            x = x_noised
+            x = q_sample
 
         x = self.x_embedder(x) + self.pos_embed  # (N, T, D), where T = H * W / patch_size ** 2
         t = self.t_embedder(t)                   # (N, D)
