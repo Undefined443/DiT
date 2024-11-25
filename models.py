f174ded9f50b136d187a1819f70fccbe77d822be
Xiao Li <xiaoli3397@gmail.com>
2024-11-25 20:22:03 +0800

    update

--- models.py
+++ models.py
@@ -253,7 +253,7 @@ class DiT(nn.Module):
         """
         # https://github.com/openai/glide-text2im/blob/main/notebooks/text2im.ipynb
         if log_variance is not None:
-            with open('in_x0.log', 'a') as file:
+            with open('in_x.log', 'a') as file:
                 mean_value = torch.mean(x)
                 file.write(f"{mean_value.item()}\n")  # 打印未加噪的 x 的均值
             with open('in_log_variance.log', 'a') as file:
@@ -266,9 +266,12 @@ class DiT(nn.Module):
             nonzero_mask = (
                 (t != 0).float().view(-1, *([1] * (len(x.shape) - 1)))
             )  # no noise when t == 0
-            x = x + nonzero_mask * torch.exp(0.5 * log_variance) * noise
+            x = x + nonzero_mask * torch.exp(0.5 * log_variance) * noise  # 加噪
+            with open('in_xt.log', 'a') as file:
+                mean_value = torch.mean(x)
+                file.write(f"{mean_value.item()}\n")  # 打印加噪后的 xt
 
-        with open('in_xt.log', 'a') as file:
+        with open('in_model_input.log', 'a') as file:
             mean_value = torch.mean(x)
             file.write(f"{mean_value.item()}\n")     # 打印加噪后的 x 的均值
 
